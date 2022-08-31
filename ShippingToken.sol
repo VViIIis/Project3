605fe093f2c30e256331db5c5eab64ea7edd44c2
olivia <oliviabheitz@gmail.com>
2022-08-30 21:11:03 -0700

    edited sol file

--- ShippingToken.sol
+++ ShippingToken.sol
@@ -14,12 +14,24 @@ contract ShippingToken is ERC721Full {
     string public symbol = "SHIP";
     bool approvetransfer = false;
     uint public amount;
+    uint256 tokenId;
+    string tokenURI;
     
-    constructor(address TokenAdmin, uint256 TokenAmount ) public ERC721Full("Certificate", "CERT"){
-        admin = TokenAdmin;
-        amount = TokenAmount;
+    constructor(address TokenAdmin, uint) public ERC721Full("Certificate", "CERT"){
+        admin = TokenAdmin; 
+        amount = 100; /*value of order for insurance*/
         }
-    
+
+    struct Shipment {
+    string shipment_name;
+    string origin_address;
+    string destination_address;
+    uint256 shipment_weight;
+    uint256 num_packages;
+    string plist_uri;
+    string insurance_policy_uri;
+    }
+
     function createToken(string memory tokenURI) public {
         uint256 tokenCount=totalSupply();
         uint256 newTokenId=tokenCount;
@@ -36,10 +48,47 @@ contract ShippingToken is ERC721Full {
         return TokenOwner
     }*/
 
+    mapping(bytes32 => string) public requestIdToTokenURI;
+
+    function setTokenURI(uint256 tokenId, string memory tokenURI) public {
+        require (_isApprovedOrOwner(msg.sender)(), tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        
+    }
+
+    /*function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }*/
+
+    mapping(uint256 => Shipment) public TotalShipment;
+
+    function registerShipment(
+        address owner,
+        string memory shipment_name,
+        string memory origin_address,
+        string memory destination_address,
+        uint256 shipment_weight,
+        uint256 num_packages,
+        string memory plist_uri,
+        string memory insurance_policy_uri
+    ) public returns (uint256) {
+        uint256 tokenId = totalSupply();
+
+        _mint(owner, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+
+        TotalShipment[tokenId] = Shipment(TokenOwner, shipment_name, destination_address, num_packages);
+
+        return tokenId;
+    }
+
     function getTokenOwner(address TokenOwner, uint256 newTokenId) external view returns (address){
         return ShippingToken(TokenOwner).ownerOf(newTokenId);
     }
     
+    function balanceOf(address owner) public returns (uint256 balance)
+
     function approve(uint tokens) public returns (bool success) {
         require (msg.sender == admin);
          /* makes it so only admin (3rd party) can call*/
@@ -48,28 +97,36 @@ contract ShippingToken is ERC721Full {
 
         /* is there a third party? or is it sender or receiver approving? */
     }
+
 /* have a set address fo approver, have that address call the transfer function, only that address can call this function*/ 
 
 
     /*function transferFrom(address from, address to, uint tokens) public returns (bool success);*/
 
-    function bid(uint256 new_bid, uint256 bid_token_id) public {
+  /*  function bid(uint256 new_bid, uint256 bid_token_id) public {
         require(new_bid>current_bid[bid_token_id], "Must be higher than current bid.");
         current_bid[bid_token_id]=new_bid;
     }
 
     function getCurrentBid(uint256 bid_token_id) public view returns(uint256) {
-        return current_bid[bid_token_id];
+        return current_bid[bid_token_id]; */
     
-     function transfer(msg.sender, address recipient, uint amount) public payable {
-        require approvetransfer == true;
+    /* function transfer(msg.sender, address recipient, uint amount) public payable {
+        require approvetransfer = true;
         uint amount = msg.value * exchange_rate;
         balances[msg.sender] = balances[msg.sender].sub(amount);
         balances[recipient] = balances[recipient].add(amount);
         string message = "Congratulations, transaction complete";
         approvetransfer = false;
         return balances, message
-    }
+    } */
+
+    /* address seller = ownerOf(_tokenId);
+        _transfer(seller, msg.sender, _tokenId);
+        tokenIdToPrice[_tokenId] = 0; // not for sale anymore
+        payable(seller).transfer(msg.value); // send the ETH to the seller
+        emit NftBought(seller, msg.sender, msg.value);
+    } /*
 
     /*function verify(address recipient, address sender, uint) public {
         receiver address;
@@ -87,10 +144,4 @@ contract ShippingToken is ERC721Full {
  old owner or future owner or project owner
 can we require (receiver == receiver)
  */
-
-
-
 }
-
-/* deploy on remix, check balances on ganache/*
-
